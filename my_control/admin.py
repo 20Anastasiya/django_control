39d24347bc823464bea2b516e3a7a5a2c1feb323
Anastasia Bereznevich <nastena.bereznevich@mail.ru>
2022-12-16 00:38:50 +0300

    admin end (task 2)

--- my_control/admin.py
+++ my_control/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 
 from my_control.models import Department, Employee
 
-admin.site.register(Department)
-admin.site.register(Employee)
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('fio', 'position', 'salary', 'department')
+    list_filter = ('fio', 'salary')
+    search_fields = ('fio', 'position')
+
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ('name', )
+
+
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Department, DepartmentAdmin)
